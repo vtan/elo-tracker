847ffc154826ec50c4bcbc1f6fec39b53dc6c4f8
Viktor Tanyi <tanyi.viktor@gmail.com>
2020-03-21 13:48:27 +0100

    Separate games by year

--- src/main/typescript/components/GameTable.tsx
+++ src/main/typescript/components/GameTable.tsx
@@ -1,19 +1,19 @@
 import * as AppReducer from "./AppReducer"
 import { DarkGameCell, LightGameCell } from "./GameCell"
 import { LinkButton } from "./LinkButton"
-import { formatDate } from "../Util"
+import { RatedGame } from "../Game"
 
+import { DateTime } from "luxon"
 import * as React from "react"
 import styled from "styled-components"
 
 interface Props {
   dispatch: AppReducer.Dispatch,
-  state: AppReducer.State
+  games: ReadonlyArray<RatedGame>,
+  selectedPlayer?: string
 }
 
-export function GameTable({ dispatch, state }: Props) {
-  const { games, selectedPlayer } = state
-
+export function GameTable({ dispatch, games, selectedPlayer }: Props) {
   const reverseGames = [ ...games ].reverse()
 
   return <div>
@@ -30,33 +30,53 @@ export function GameTable({ dispatch, state }: Props) {
             const ratingDiff2 = rating2 - (previousRatings[game.player2] || { rating: 1500 }).rating
             const signumToSymbol = (x: number) => x < 0 ? "↘" : "↗"
             const selected = game.player1 === selectedPlayer || game.player2 === selectedPlayer
-            return <Row key={game.playedAt} selected={selected}>
-              <td>{ formatDate(game.playedAt) }</td>
-              <DarkGameCell>
-                <Player
-                  winner={ game.score1 > game.score2 }
-                  onClick={ () => dispatch({ type: "playerToggled", player: game.player1 }) }>
-                    {game.player1}
-                </Player>
-              </DarkGameCell>
-              <DarkGameCell>{rating1.toFixed(0)} {signumToSymbol(ratingDiff1)}{Math.abs(ratingDiff1).toFixed(0)}</DarkGameCell>
-              <DarkGameCell style={{textAlign: "right"}}><strong>{game.score1}</strong></DarkGameCell>
-              <LightGameCell><strong>{game.score2}</strong></LightGameCell>
-              <LightGameCell>{rating2.toFixed(0)} {signumToSymbol(ratingDiff2)}{Math.abs(ratingDiff2).toFixed(0)}</LightGameCell>
-              <LightGameCell>
-                <Player
-                  winner={ game.score2 > game.score1 }
-                  onClick={ () => dispatch({ type: "playerToggled", player: game.player2 }) }>
-                    {game.player2}
-                </Player>
-              </LightGameCell>
-              <td><LinkButton onClick={ () => AppReducer.deleteGame(dispatch, game) }>✗</LinkButton></td>
-            </Row>
+
+            const playedAt = DateTime.fromISO(ratedGame.game.playedAt, { zone: "utc" })
+            const newYear =
+              index === games.length - 1 ||
+              DateTime.fromISO(games[index + 1].game.playedAt, { zone: "utc" }).year !== playedAt.year
+
+            return <>
+              { newYear
+                  ? <tr key={`separeator-${playedAt.year}`}>
+                    <th colSpan={3} />
+                    <YearSeparator colSpan={2}>{playedAt.year}</YearSeparator>
+                    <th colSpan={3} />
+                    </tr>
+                  : null
+              }
+              <Row key={game.id} selected={selected}>
+                <td>{ playedAt.toFormat("LLL d") }</td>
+                <DarkGameCell>
+                  <Player
+                    winner={ game.score1 > game.score2 }
+                    onClick={ () => dispatch({ type: "playerToggled", player: game.player1 }) }>
+                      {game.player1}
+                  </Player>
+                </DarkGameCell>
+                <DarkGameCell>{rating1.toFixed(0)} {signumToSymbol(ratingDiff1)}{Math.abs(ratingDiff1).toFixed(0)}</DarkGameCell>
+                <DarkGameCell style={{textAlign: "right"}}><strong>{game.score1}</strong></DarkGameCell>
+                <LightGameCell><strong>{game.score2}</strong></LightGameCell>
+                <LightGameCell>{rating2.toFixed(0)} {signumToSymbol(ratingDiff2)}{Math.abs(ratingDiff2).toFixed(0)}</LightGameCell>
+                <LightGameCell>
+                  <Player
+                    winner={ game.score2 > game.score1 }
+                    onClick={ () => dispatch({ type: "playerToggled", player: game.player2 }) }>
+                      {game.player2}
+                  </Player>
+                </LightGameCell>
+                <td><LinkButton onClick={ () => AppReducer.deleteGame(dispatch, game) }>✗</LinkButton></td>
+              </Row>
+            </>
         } ) }
       </tbody>
     </table>
   </div>
 }
+
+const YearSeparator = styled.th`
+  padding: 0.5rem 0;
+`
 
 const Row = styled.tr<{ selected: boolean }>`
   background-color: ${props => props.selected ? "#edf96c" : "transparent"};
